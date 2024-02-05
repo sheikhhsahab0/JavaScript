899113dec10169c3bd37301c48ef5012e804ace4
sheikhhsahab0 <arbazhaider.java@gmail.com>
2024-02-05 06:59:16 +0530

    config done

---
+++ Day1/01_variable.js
@@ -0,0 +1,21 @@
+const userId=1234;
+let userName='Arbaz';
+let userEmail='arbaz.gmail.com';
+var userPassword='123456';
+userCity='Siwan';
+let userState;
+
+// userId=2346; not allowed
+
+userName='sheikhh.sahab';
+userEmail='sheikhhsahab@gamil.com';
+userPassword='728205';
+userCity='Bangalore';
+
+console.log(userId);
+
+/*
+Var has some issues in Block Scope and Functional Scope
+*/
+
+console.table([userName, userEmail, userPassword, userCity, userState])
---
+++ Day1/02_datatypes.js
@@ -0,0 +1,34 @@
+'use strict'; // treat all js code as newer version
+
+// alert( 3 + 3 ); // using nodejs not browser and in nodejs it will show error but in browser it will not show error
+
+console.log(3 + 3);
+console.log('Arbaz');
+
+// code readability should be high
+
+/* 
+    -------Datatypes----------
+
+    --------Primitive Datatypes-------
+
+    number => 2 to power 53
+    bigint
+    string => "" or ''
+    boolean => true/false
+    null => standalone value
+    undefined => shows values have not assigned yet
+    symbol => when we talk unique we talk about symbol
+*/
+
+let name = 'Arbaz';
+let age = 24;
+let isLoggedIn= false;
+let state;
+let city=null;
+
+// console.log(typeof 'name');
+// console.log(typeof age);
+// console.log(typeof isLoggedIn);
+// console.log(typeof state);
+console.log(typeof city);
