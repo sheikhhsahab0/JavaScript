6144c072df54d8c3b7e3feca64e8ba6226fd426e
sheikhhsahab0 <arbazhaider.java@gmail.com>
2024-02-07 06:11:51 +0530

    container done

---
+++ Day2/04_operation.js
@@ -0,0 +1,14 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 = 'Hello';
+let str2 = ' Arbaz';
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log('1' + 2);
+console.log(1 + '2');
+console.log('1' + '2');
+console.log('1' + 2 + 2);
+console.log(1 + 2 + '2');
